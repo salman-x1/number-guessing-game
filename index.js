4bec50da11d492db2798fcd057c42b522f6168ad
salman-x1 <salmannazir2001@gmail.com>
2024-05-23 21:02:17 +0500

    Updated

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const minNum = 50;
 const maxNum = 100;
 const answer = Math.floor(Math.random() * (maxNum - minNum + 1) + minNum);
+
 console.log(answer);
 
 let guess;
+let attempts = 0;  //Attempts for guessing the number
 let running = true;
 while (running) {
   guess = window.prompt("Guess Number between 50 t0 100");
@@ -13,15 +15,16 @@ while (running) {
     window.alert("Enter a number");
   } else if (guess < minNum || guess > maxNum) {
     window.alert("Enter a valid number between 50 t0 100");
-  } 
-  else {
+  } else {
     attempts++;
     if (guess < answer) {
       window.alert("Answer is too low");
     } else if (guess > answer) {
       window.alert("Answer is too High");
     } else {
-      window.alert(`You guessed it right, ${answer}, It took you ${attempts} attempts`);
+      window.alert(
+        `You guessed it right, ${answer}, It took you ${attempts} attempts`
+      );
       running = false;
     }
   }
---
+++ readMe.md
@@ -0,0 +1,3 @@
+Github Live Link of this project:
+
+https://salman-x1.github.io/number-guessing-game/
